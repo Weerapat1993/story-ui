8871e78110da834ee41046f6582cff3cab333b08
Weerapat Thawatchoketawee <weerapat@central.tech>
2018-11-01 15:13:45 +0700

    Add Menu

--- src/components/AppDrawer/AppDrawer.js
+++ src/components/AppDrawer/AppDrawer.js
@@ -1,6 +1,9 @@
-import React from 'react'
-import { func, element, node, oneOfType, string } from 'prop-types'
+import React, { PureComponent, Fragment } from 'react'
+import { func, element, node, oneOfType, string, array } from 'prop-types'
 import styled from 'styled-components'
+import posed, { PoseGroup } from 'react-pose'
+import { tween } from 'popmotion'
+import { Button } from '../Button'
 
 const AppDrawerHeader = styled.header`
   background: ${props => props.bgColor};
@@ -15,12 +18,124 @@ const AppDrawerHeader = styled.header`
   right: 0;
   height: 60px;
 `
+const Flex = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+`
+const PageWihte = styled.div`
+  position: fixed;
+  height: 100%;
+  top: 0;
+  left: 0;
+  z-index: 2;
+  background-color: white;
+  width: 0%;
+  display: block;
+  overflow-x: hidden;
+  border-right: 1px solid #d1d1d1;
+`
+const Drawer = posed(PageWihte)({
+  enter: { 
+    width: '100%',
+    transition: tween
+  },
+  exit: { 
+    width: '0%',
+    transition: tween
+  },
+  init: {
+    width: '0%',
+  }
+});
 
-const AppDrawer = (props) => (
-  <AppDrawerHeader {...props}>
-    {props.title}
-  </AppDrawerHeader>
-)
+const Shade = posed.div({
+  enter: { 
+    opacity: 1,
+    transition: tween
+  },
+  exit: { 
+    opacity: 0,
+    transition: tween
+  },
+  init: {
+    opacity: 0,
+  }
+});
+
+const DrawerItem = styled.div`
+  cursor: pointer;
+  padding: 20px;
+  border-top: 1px solid #d1d1d1;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+  display: block;
+  overflow-x: hidden;
+  min-width: 300px;
+  &:hover {
+    background: rgba(0, 0, 0, 0.2);
+  }
+`
+
+class AppDrawer extends PureComponent {
+  static propTypes = {
+    data: array,
+    onMenu: func,
+    onClick: func,
+  }
+
+  static defaultProps = {
+    data: [],
+    onMenu: () => null,
+    onClick: () => null,
+  }
+
+  state = { visible: false }
+
+  setVisible = (value) => {
+    this.setState({ visible: value })
+  }
+
+  handleClick = (item) => {
+    this.props.onClick(item)
+    this.setVisible(false)
+  } 
+
+  render() {
+    const { title, data } = this.props
+    const { visible } = this.state
+    return (
+      <Fragment>
+        <AppDrawerHeader {...this.props}>
+          <Button onClick={() => this.setVisible(true)}>Menu</Button>
+          <Flex />
+          {title}
+          <Flex />
+          <Button onClick={() => this.setVisible(true)}>Menu</Button>
+        </AppDrawerHeader>
+        <PoseGroup>
+          {
+            visible && (
+              <Drawer key='drawer'>
+                <Shade key='drawer'>
+                  <div style={{ textAlign: 'right' }}>
+                    <Button onClick={() => this.setVisible(false)}>X</Button>
+                  </div>
+                  {
+                    data.map(item => (
+                      <DrawerItem onClick={() => this.handleClick(item)} key={item.key}>{item.text}</DrawerItem>
+                    ))
+                  }
+                </Shade>
+              </Drawer>
+            )
+          }
+        </PoseGroup>
+      </Fragment>
+    )
+  }
+}
 
 AppDrawer.propTypes = {
   bgColor: string,
--- stories/app-drawer.stories.js
+++ stories/app-drawer.stories.js
@@ -5,6 +5,49 @@ import { configStories } from './config'
 const BG_COLOR = 'linear-gradient(258deg, #83409e, #4c1e64)'
 const LOGO = 'https://dfmukn6hiiik1.cloudfront.net/static/version1541005601/frontend/Central/default/th_TH/images/logo.svg'
 
+const data = [
+  {
+    key: 1,
+    link: '/home',
+    text: 'ทีวี / บันเทิงภายในบ้าน'
+  },
+  {
+    key: 2,
+    link: '/home',
+    text: 'เครื่องใช้ไฟฟ้าภายในบ้าน'
+  },
+  {
+    key: 3,
+    link: '/home',
+    text: 'แอร์ / เครื่องฟอกอากาศ'
+  },
+  {
+    key: 4,
+    link: '/home',
+    text: 'เครื่องใช้ไฟ้ฟ้าขนาดเล็ก'
+  },
+  {
+    key: 5,
+    link: '/home',
+    text: 'มือถือ / แทบเล็ต'
+  },
+  {
+    key: 6,
+    link: '/home',
+    text: 'กล้อง'
+  },
+  {
+    key: 7,
+    link: '/home',
+    text: 'คอมพิวเตอร์ / ออฟฟิศ'
+  },
+  {
+    key: 8,
+    link: '/home',
+    text: 'แกตเจ็ด & สมาร์ทเทคโนโลยี',
+  },
+]
+
 const Title = () => (
   <a href="https://www.powerbuy.co.th/th/" title="Power Buy" target="_blank" rel="noopener noreferrer">
     <img src={LOGO} alt="Power Buy" width="165" height="44" />
@@ -14,7 +57,9 @@ const Title = () => (
 const stories = configStories(() => (
   <AppDrawer 
     bgColor={BG_COLOR} 
-    title={<Title />} 
+    title={<Title />}
+    data={data}
+    onClick={item => console.log(item)}
   />
 ),
   'App Drawer', 
